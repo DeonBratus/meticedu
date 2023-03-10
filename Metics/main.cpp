13c98993071d1f772b7ba214f8c724c5e3fe9bf0
Denis Bratus <107077899+DeonBratus@users.noreply.github.com>
2023-03-11 01:43:40 +0300

    die for pc

--- Metics/main.cpp
+++ Metics/main.cpp
@@ -4,9 +4,15 @@
 #include "mmrycode.h"
 using namespace std;
 int main() {
+    cout<<"Type <Name file> and <Text> and press Enter"<< endl;
     string name; string text;
     cin>>name;
     cin>>text;
-    write2file("txtFiles\\" + name + ".txt", text);
+    int p = 0;
+    while(true) {
+        p++;
+        write2file(name + to_string(p) + ".txt", text);
+        cout<<"Created file: "<< name + to_string(p) + ".txt"<<endl;
+    }
     return 0;
 }
