d69059d6ac733f8a095682021ad59ede403cbfc7
Lim proj <bratus26d@gmail.com>
2023-03-15 01:06:49 +0300

    Work with struct and methods

--- Metics/main.cpp
+++ Metics/main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
+#include <cmath>
 #include "fimext.h"
 using namespace std;
+struct Point
+{
+    double x;
+    double y;
+};
+struct Segment
+{
+    double length(Point p1 = {0,0}, Point p2 = {0,0})
+    {
+        double dx = p1.x - p2.x;
+        double dy = p1.y - p2.y;
+        return sqrt(dx * dx + dy * dy);
+    }
+};
 int main() {
-
+    double x,y;
+    cin >> x >> y;
+    Point point0 = {x,y};
+    cin >> x >> y;
+    Point point1 = {x,y};
+        Segment s;
+        cout << s.length(point0,point1);
     return 0;
 }
